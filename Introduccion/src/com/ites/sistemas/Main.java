6d175b02173f633239bebf9b634186b702dbdeed
Jacob Green <jacob@ellipz.io>
2022-02-09 22:06:30 -0700

    Declaracion de variables en java.

---
+++ Introduccion/src/com/ites/sistemas/Main.java
@@ -0,0 +1,20 @@
+package com.ites.sistemas;
+
+
+public class Main {
+
+    // variables
+    int a = 5;
+    double b = 5.0;
+    boolean c = true;
+    char d = 'a';
+    Integer x = 5;
+    Double y = 5.0;
+    Boolean w = false;
+    Character z = 'a';
+    String cadena = "Hola mundo!!!";
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!!!");
+    }
+}
