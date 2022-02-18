6afc9f950b364088ba47657d600ea7d0dd43443d
Jacob Green <jacob@ellipz.io>
2022-02-17 20:09:54 -0700

    Agregar ComboBox

--- Unidad1/IntroJavaFx/src/main/java/com/example/ejemplojavafx/HelloApplication.java
+++ Unidad1/IntroJavaFx/src/main/java/com/example/ejemplojavafx/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Mi ventana");
         stage.setScene(scene);
         stage.show();
--- Unidad1/IntroJavaFx/src/main/java/com/example/ejemplojavafx/HelloController.java
+++ Unidad1/IntroJavaFx/src/main/java/com/example/ejemplojavafx/HelloController.java
@@ -1,12 +1,20 @@
 package com.example.ejemplojavafx;
 
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private TextField txt_nombre;
     @FXML
@@ -21,6 +29,8 @@ public class HelloController {
     private Button btn_cancelar;
     @FXML
     private Button btn_guardar;
+    @FXML
+    private ComboBox<String> cmb_pais;
 
     public void onGuardarButtonClick(ActionEvent actionEvent) {
         String nombre = txt_nombre.getText();
@@ -28,5 +38,20 @@ public class HelloController {
         String apellidoM = txt_apellidoM.getText();
         String email = txt_email.getText();
         int edad = Integer.parseInt(txt_edad.getText());
+    }
+    public void onCancelarButtonClick(ActionEvent actionEvent) {
+        Button boton = (Button) actionEvent.getSource();
+        Scene escena = boton.getScene();
+        Stage stage = (Stage) escena.getWindow(); // casting
+        stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cmb_pais.setPromptText("Seleccione un pais");
+        ObservableList<String> listaDePaises = cmb_pais.getItems();
+        listaDePaises.add("Mexico");
+        listaDePaises.add("Brasil");
+        listaDePaises.add("Italia");
     }
 }
