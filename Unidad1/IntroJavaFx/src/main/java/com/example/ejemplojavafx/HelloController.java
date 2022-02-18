be0d7fdae7ad040f37a62d25f8984767c26de704
Jacob Green <jacob@ellipz.io>
2022-02-17 21:02:58 -0700

    Agregar ComboBox

--- Unidad1/IntroJavaFx/src/main/java/com/example/ejemplojavafx/HelloController.java
+++ Unidad1/IntroJavaFx/src/main/java/com/example/ejemplojavafx/HelloController.java
@@ -1,12 +1,20 @@
 package com.example.ejemplojavafx;
 
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private TextField txt_nombre;
     @FXML
@@ -21,6 +29,8 @@ public class HelloController {
     private Button btn_cancelar;
     @FXML
     private Button btn_guardar;
+    @FXML
+    private ComboBox<String> cmb_carrera;
 
     public void onGuardarButtonClick(ActionEvent actionEvent) {
         String nombre = txt_nombre.getText();
@@ -28,5 +38,19 @@ public class HelloController {
         String apellidoM = txt_apellidoM.getText();
         String email = txt_email.getText();
         int edad = Integer.parseInt(txt_edad.getText());
+    }
+    public void onCancelarButtonClick(ActionEvent actionEvent) {
+        Button boton = (Button) actionEvent.getSource(); // casting
+        Scene escena = boton.getScene();
+        Stage stage = (Stage) escena.getWindow();
+        stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> lista = cmb_carrera.getItems();
+        lista.add("Ingenieria en Sistemas Computacionales");
+        lista.add("Arquitectura");
+        lista.add("Contaduria");
     }
 }
