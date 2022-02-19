3c3cd72b695564d091582bbba27e41e172c39da9
Jacob Green <jacob@ellipz.io>
2022-02-18 20:53:23 -0700

    Mostrar alerta en cancelar

--- Unidad1/IntroJavaFx/src/main/java/com/example/ejemplojavafx/HelloController.java
+++ Unidad1/IntroJavaFx/src/main/java/com/example/ejemplojavafx/HelloController.java
@@ -6,12 +6,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
@@ -38,17 +37,31 @@ public class HelloController implements Initializable {
         String apellidoM = txt_apellidoM.getText();
         String email = txt_email.getText();
         int edad = Integer.parseInt(txt_edad.getText());
+        String carrera = cmb_carrera.getValue();
+        System.out.println(carrera);
     }
     public void onCancelarButtonClick(ActionEvent actionEvent) {
-        Button boton = (Button) actionEvent.getSource();
-        Scene escena = boton.getScene();
-        Stage stage = (Stage) escena.getWindow(); // casting
-        stage.close();
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Confirmar salida");
+        alerta.setHeaderText("Deseas salir del programa?");
+        alerta.setContentText("Guarde sus datos antes de salir,\n" +
+                "de lo contrario se perderan");
+        Optional<ButtonType> respuesta = alerta.showAndWait();
+        if(respuesta.isPresent() && respuesta.get() == ButtonType.OK) {
+            // cerramos el programa
+            Button boton = (Button) actionEvent.getSource();
+            Scene escena = boton.getScene();
+            Stage stage = (Stage) escena.getWindow(); // casting
+            stage.close();
+        } else {
+            // click en cancelar, hacemos otra cosa aca
+            System.out.println("Cancelando...");
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        cmb_carrera.setPromptText("Seleccione un pais");
+        cmb_carrera.setPromptText("Seleccione una carrera");
         ObservableList<String> listaDeCarreras = cmb_carrera.getItems();
         listaDeCarreras.add("Ingenieria en sistemas computacionales");
         listaDeCarreras.add("Arquitectura");
