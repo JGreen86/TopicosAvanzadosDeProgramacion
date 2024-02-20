8f3d8e1129691157b35f01f9e9b1492e06b83179
Jacob Green <jego.86@hotmail.com>
2024-02-20 12:43:27 -0700

    Reestructurar repositorio.

---
+++ Unidad1/EjemplosJavaFX/src/main/java/com/ites/sistemas/ejemplosjavafx/MainController.java
@@ -0,0 +1,33 @@
+package com.ites.sistemas.ejemplosjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class MainController {
+
+    @FXML
+    private MenuBar menuBarMain;
+
+    public void onMenuItemFrmBasicoClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) menuBarMain.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/formulario-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Formulario básico");
+        stage.setScene(scene);
+    }
+
+    public void onMenuItemTableViewClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/table-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Table view básico");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
